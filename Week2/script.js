ee408aca127bd43e89658a1da74d2d8d9bdb3475
lynScott03 <158178536+lynScott03@users.noreply.github.com>
2024-04-25 21:52:20 +0100

    Delete Week2/script.js

--- Week2/script.js
+++
@@ -1,8 +0,0 @@
-const colourPicker = document.getElementById('colourPicker');
-const previewBox = document.getElementById('previewBox');
-
-colourPicker.addEventListener('input', function() {
-    const colour = colourPicker.ariaValueMax;
-    previewBox.style.backgroundColour = colour;
-
-});
